bdeabd4d312e43671ce589bdaf0ce9b57ba60683
Leonardo-Rendano <62267208+Leonardo-Rendano@users.noreply.github.com>
2020-05-27 03:12:00 -0300

    Commit inicial do repositório

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { FaHeart } from 'react-icons/fa';
+import './styles.css';
+import Img from './assets/coffee4.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default function App() {
+
+  const [coffee, setCoffee] = useState(0);
+
+  function addCoffee() {
+    setCoffee(coffee +1);
+  }
+
+  function removeCoffee() {
+    if (coffee !== 0) {
+      setCoffee(coffee -1);
+    }
+  }  
+  
+
+  return(
+    <div className="container">
+    <div className="main_div">
+      <h1>Coffee Lover <FaHeart size={35} /> </h1>
+      <h2> {coffee} cafés!</h2>
+      <div className="btn">
+        <button type="button" onClick={removeCoffee}>Ops! Errei na conta.</button>
+        <button type="button" onClick={addCoffee}>Opa! Mais um!</button>
+      </div>
+      <div className="coffee_img">
+        <img src={Img} alt='coffee' />
+      </div>
     </div>
-  );
+    </div>
+  )
 }
-
-export default App;
